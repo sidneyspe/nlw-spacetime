33a7d01a577c1338e180cf6594107f9924858ee0
Sidney Pimentel <sidneyspe@gmail.com>
2024-02-22 20:02:15 -0300

    mobile: init front

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,19 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
 
-const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './global.css' })
+  const { transformer, resolver } = config
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  }
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  }
+
+  return config
+})()
