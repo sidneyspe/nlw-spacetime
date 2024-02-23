8e12cacea922343ffbaf459c90288184fbabfff6
Sidney Pimentel <sidneyspe@gmail.com>
2024-02-23 14:53:16 -0300

    web: init auth

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://rocketseat.com.br"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
