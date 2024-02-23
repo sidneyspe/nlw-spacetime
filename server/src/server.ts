2a520f7f99efdfa7dbb39ecbfdd5b4e3ab63de46
Sidney Pimentel <sidneyspe@gmail.com>
2024-02-23 11:29:07 -0300

    server: init auth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // ['https://localhost:3333]
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(memoriesRoutes)
